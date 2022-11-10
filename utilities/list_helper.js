b1286f06e27174e611efe498ce32bbc47b59eb50
chheangg <balldremz@gmail.com>
2022-11-10 12:44:08 +0700

    Complete part 4.6 on finding most blogged author

--- utilities/list_helper.js
+++ utilities/list_helper.js
@@ -6,7 +6,32 @@ const totalLikes = (blogs) => {
   return blogs.reduce((likes, blog) => likes + blog.likes, 0)
 }
 
+const mostBlogs = (blogs) => {
+  let countArr = [];
+  const authorObject = blogs.reduce((authors, blog) => {
+    console.log(authors)
+    if (!authors[blog.author]) {
+      authors[blog.author] = 1;
+    } else {
+      authors[blog.author] += 1;
+    }
+    return authors;
+  }, {})
+
+  for (const author in authorObject) {
+    countArr.push({
+      author: author,
+      blogs: authorObject[author], 
+    })
+  }
+
+  const mostBlogged = countArr.reduce((prev, curr) => Math.max(prev.blogs, curr.blogs) == prev.blogs ? prev : curr)
+
+  return mostBlogged;
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  mostBlogs,
 }
